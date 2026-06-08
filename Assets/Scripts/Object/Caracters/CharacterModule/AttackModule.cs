3cdebed2a5a2002578afd1e9c496b3037b1395bf
GJ <a01082316650@gmail.com>
2026-06-08 16:55:25 +0900

    학원 - 어텍모듈 스테이터스모듈 HP모듈

--- Assets/Scripts/Object/Caracters/CharacterModule/AttackModule.cs
+++ Assets/Scripts/Object/Caracters/CharacterModule/AttackModule.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class AttackModule : CharacterModule
 {
-    [SerializeField] private float _attackPower = 1f;
-    public float AttackPower => _attackPower;
+    [SerializeField] private float _attackDamage = 1f;
+    public float AttackDamage => _attackDamage;
 
-    [SerializeField] private float _magicPower = 1f;
-    public float MagicPower => _magicPower;
+    [SerializeField] private float _avilityPower = 1f;
+    public float AvilityPower => _avilityPower;
 
     [SerializeField] private float _attackSpeed = 10f;
     public float AttackSpeed => _attackSpeed;
@@ -17,12 +17,49 @@ public class AttackModule : CharacterModule
     [SerializeField] private float _criticalChance = 0f;
     public float CriticalChance => _criticalChance;
 
+    [SerializeField] private float _defense = 0f;
+    public float Defense => _defense;
+
+    [SerializeField] private float _adPenetration = 0f;
+    public float ADPenetration => _adPenetration;
+
+    [SerializeField] private float _apPenetration = 0f;
+    public float APPenetration => _apPenetration;
+
+    [SerializeField] private float _buff = 0f;
+    public float Buff => _buff;
+
+    public float AD(float adAddBuff, float adMultiplierBuff)
+    {
+        float adDamage;
+
+        adDamage = _attackDamage + adAddBuff * adMultiplierBuff;
+
+        return adDamage;
+    }
+
+    public float APDeal(buff)
+    {
+
+    }
+
     public bool CriticalRandom()
     {
         int criticalvalue = Random.Range(1, 100);
 
         if (_criticalChance <= criticalvalue) return true;
         else return false;
+    }
+
+    public float CriticalDamage()
+    {
+        float criticalDeal;
+        if (_criticalChance == 0f) return 1f;
+
+        if (CriticalRandom()) criticalDeal = (_attackDamage + _avilityPower) * _criticalMultiple;
+        else return criticalDeal = (_attackDamage + _avilityPower);
+
+        return criticalDeal;
     }
 
     public float FinalDamage()
@@ -42,6 +79,12 @@ public class AttackModule : CharacterModule
     {
         HitPointModule target = null;
         target.TakeDamage(gameObject, Owner.Controller, FinalDamage());
+    }
+
+    public float Defence()
+    {
+        float def = (150f / (150f + _defense));
+        return def;
     }
 
 }
--- Assets/Scripts/Object/Caracters/CharacterModule/StatusModule.cs
+++ Assets/Scripts/Object/Caracters/CharacterModule/StatusModule.cs
@@ -1,16 +1,54 @@
+using Unity.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class StatusModule : MonoBehaviour
+public class StatusModule : CharacterModule
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    HitPointModule HP;
+    AttackModule Damage;
+
+    private float _level;
+    public float Level => _level;
+
+    private float _strength;
+    public float Strength => _strength;
+    GetMaxHP => _maxHP + (_strength* 10)
+
+    private float _agility;
+    public float agility => _agility;
+
+    private float _intelligence;
+    public float Intelligence => _intelligence;
+
+    public void LevelSystem()
     {
-        
+        if (_level >= 50) _level = 50;
+        statusPoint();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void statusPoint()
     {
-        
+    }
+
+    // public void STR(float addSTR)
+    // {
+    //     if (HP == null) return;
+    //     _strength += addSTR;
+    //     float addHP = (_strength - beforeSTR) * 10;
+    //     HP.MaxIncreaseHP(addHP);
+    //     beforeSTR = _strength;
+    // }
+
+    public void buttonSTR(float addSTR)
+    {
+        HP.MaxIncreaseHP(addSTR));
+    }
+    
+    //if (Damage == null) return;
+    //    float addDamage = (_strength - beforeSTR) * 10;
+    //    Damage.
+    //
+
+
     }
 }
